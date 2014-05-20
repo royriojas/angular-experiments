c9b430e8b6d4b23ac71ef40b9d5a15cd95f35540
Roy Riojas <roy@kno.com>
2014-05-20 10:59:29 -0700

    Example of several controllers

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,6 @@
   var myApp = angular.module( 'myApp', [] );
 
   myApp.controller( 'HomeController', function ( $scope, $element ) {
-
     var me = this;
     me.name = 'Roy is awesome!';
 
@@ -14,6 +13,22 @@
       console.log( 'Hello : ' + me.name );
       console.log( me === this );
       me.name = 'Roy is even more awesome!';
+      console.log( $element );
+      console.log( $scope );
+      console.log( me );
+      //console.log($scope === me);
+    };
+  } );
+
+
+  myApp.controller( 'HomeController2', function ( $scope, $element ) {
+    var me = $scope;
+    me.name = 'Roy is awesome!';
+
+    me.sayHello = function () {
+      console.log( 'Hello : ' + me.name );
+      console.log( me === this );
+      me.name = 'Roy is even more awesome from HomeController2!';
       console.log( $element );
       console.log( $scope );
       console.log( me );
