6999eafc726ae447107cedb3cf6b9c2a298b4850
Roy Riojas <roy@kno.com>
2014-05-20 13:37:12 -0700

    Added response back

--- grunt-deps/config/tasks/browserify.js
+++ grunt-deps/config/tasks/browserify.js
@@ -7,12 +7,18 @@ module.exports = function(grunt) {
       src: [ './src/js/main.js' ],
       // the destination
       dest: 'dist/js/app.js',
+      bundleOptions: {
+        debug: true
+      },
       options: {
         // change the name which will be used to include the app
         // from the browser (using require)
         // alias: [ "./src/js/app.js" ],
         exclude: [ './node_modules/jquery/dist/jquery.js', './node_modules/angular/index.js' ],
-        transform: [ 'browserify-shim', 'uglifyify' ]
+        transform: [
+          'browserify-shim'
+          //, 'uglifyify'
+        ]
       }
     }
   }
--- src/js/controller/home-controller.js
+++ src/js/controller/home-controller.js
@@ -9,6 +9,7 @@ myApp.controller( 'HomeController', [ '$scope', '$http',
       "forecast/daily?mode=json" +
       "&units=imperial&cnt=1";
 
+    $scope.loading = false;
     //  var yelpUrl = 'http://api.yelp.com/business_review_search',
     //    yelpKey = 'XXXXXXX-XXXXX'; //bepunXR8pyH2dHQsP7ZFLA
     $scope.city = "San Francisco, CA";
@@ -23,13 +24,22 @@ myApp.controller( 'HomeController', [ '$scope', '$http',
         }
       } );
 
+      $scope.loading = true;
+
       promise.then( function ( response ) {
-        //console.log( 'data', response.data );
-        $scope.weather = response.data;
+        $scope.weather = {
+          temp: response.data.list[ 0 ].temp.day,
+          description: response.data.list[ 0 ].weather[ 0 ].description
+        };
+        //$scope.weather.temp = response.data.list[0].temp.day;
       } );
 
       promise[ 'catch' ]( function ( err ) {
         console.log( 'err', err );
+      } );
+
+      promise[ 'finally' ]( function () {
+        $scope.loading = false;
       } );
 
     };
