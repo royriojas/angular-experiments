528b404a3d1c9e546cd1890de357f05955344e43
Roy Riojas <roy@kno.com>
2014-05-20 22:06:08 -0700

    Refactor

--- grunt-deps/config/aliases.js
+++ grunt-deps/config/aliases.js
@@ -1,26 +1,10 @@
 module.exports = function ( grunt ) {
   'use strict';
 
-  require( './custom-tasks.js' )( grunt );
-
   var common = require( './common.js' );
   var gruntTaskUtils = require( 'grunt-ez-frontend/lib/grunt-task-utils.js' )( grunt );
 
   var gruntTasks = {
-//    'js-reader': [
-//      'validate',
-//      'compile-templates:reader',
-//      'js-target:js-reader'
-//    ],
-//    'css-reader': [
-//      //      'validate',
-//      'css-target:css-reader'
-//    ],
-//    'css': [
-//      'cLess',
-//      'autoprefixer',
-//      'csso'
-//    ],
     'validate': [
       'jsonlint',
       'jsbeautifier',
@@ -32,58 +16,6 @@ module.exports = function ( grunt ) {
       'validate',
       'browserify'
     ]
-//    'js': [
-//      'preprocess',
-//      'uglify'
-//    ],
-//    'i18nFiles': [
-//      'i18n-from-yml',
-//      'jsbeautifier:i18n'
-//    ],
-//    'warm-up': [
-//      'validate',
-//      'i18nFiles',
-//      'i18n-to-ez-frontend',
-//      'ez-frontend'
-//    ],
-//    'create-files': [
-//      'warm-up',
-//      'copy:lib',
-//      'compile-templates',
-//      'css',
-//      'js'
-//    ],
-//    'build': [
-//      'clean',
-//      'check-kwl',
-//      'create-files',
-//      'exec:aa',
-//      // 'js-tests',
-//      'exec:docs'
-//    ],
-//    'tests': [
-//      'jsbeautifier:tests',
-//      'jsvalidate:tests',
-//      'jshint:tests',
-//      'jscs:tests',
-//      'test-targets-generator',
-//      'qunit'
-//    ],
-//    'dev': [
-//      'clean',
-//      'check-kwl',
-//      // 'exec:codepainter',
-//      'create-files',
-//      'exec:aa'
-//    ],
-//    'default': [
-//      'dev'
-//    ],
-//    'chrome-app': [
-//      'dev',
-//      'copy:chromeAppDeps',
-//      'markdown-expander'
-//    ]
   };
 
   gruntTaskUtils.registerTasks( gruntTasks );
